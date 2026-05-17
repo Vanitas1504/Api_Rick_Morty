8eb6451bc820d3e2796323d55ad144cb34421269
Carlos Soria Echeverry <vanitas1504@gmail.com>
2026-05-17 17:07:17 -0500

    pagues

--- vite.config.js
+++ vite.config.js
@@ -1,5 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
+
+export default defineConfig({
+  plugins: [react()],
+  base: '/Api_Rick_Morty/'
+})
 
 // https://vite.dev/config/
 export default defineConfig({
